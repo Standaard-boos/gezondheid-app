27f7df65cee0b2a450ad2db6530eae62a6e0ae11
Andy <496993@edu.rocmn.nl>
2019-09-12 12:58:28 +0200

    Branch and basics
    When applied this commit wil push a new branch named registratie

---
+++ application/view/pages/registratie.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="css" href="public_html\assets\css\style.css">
+    <head>
+        <title>Persoon Registeren</title>
+    </head>
+<body>
+<div class="container_1">
+<h1>Persoon Registeren</h1>
+</div>
+
+
+</body>
+</html>
