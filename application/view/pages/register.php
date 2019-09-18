6019c421ef9d08a225ea0ef68d5de9cebfdd3e90
Andy <496993@edu.rocmn.nl>
2019-09-18 09:09:03 +0200

    Changed some classes for a better css

--- application/view/pages/register.php
+++ application/view/pages/register.php
@@ -1,25 +1,40 @@
-
-<div class="container_1">
-<h1>Persoon Registeren</h1>
+<div class="container-login">
+<div class="login-header">
+    <h1>Persoon Registeren</h1>
+</div>
     <div>
-        <form>
-            <div>
-            <i class="fas fa-user-circle"></i>  
-            <input type="text" name="username" value="Gebruikersnaam"> <br><br> 
+        <form class="login-form">
+            <div class="login-flex">
+                <i class="fas fa-user-circle icons-input-left"></i>
+                <input type="text" name="name" placeholder="Gebruiksnaam" class="login-inputs" required autofocus>
             </div>
-            <input type="email" name="email" value="Email"><br>
-            <label>Wachtwoord</label><br>
-            <input type="password" name="pass" value=""><br>
+            <div class="login-flex">
+                <i class="fas fa-lock icons-input-left"></i>
+                <input type="password" name="password" class="login-inputs" placeholder="Wachtwoord" required><br>
+                <i class="fas fa-eye icons-input-right"></i>
+            </div>
         </form>
-        <form name="newForm" method="get" onSubmit="formAction(this)">
-            <label>Geslacht</label><br>
-            <input type="radio" name="gender" value="male" > Male
-            <input type="radio" name="gender" value="female"> Female<br>
-            <input type="text" name="length" value="Lengte"><br><br>
-            <input type="text" name="weight" value="Gewicht"><br><br>
-            <label>Leeftijd</label><br>
-            <input type="number" name="age" value="Leeftijd"><br><br>
-            <input type="submit" value="Registreer">
+        <form class="login-form" name="newForm" method="get" onSubmit="formAction(this)">
+            <div class="login-flex">
+            <label>Geslacht</label>
+            </div>
+            <div class="login-flex">
+                <input type="radio" name="gender" value="male" > Male
+                <input type="radio" name="gender" value="female"> Female
+            </div>
+            <div class="login-flex">
+                <input type="text" name="length" value="Lengte">
+            </div>
+            <div class="login-flex">
+                <input type="text" name="weight" value="Gewicht">
+            </div>
+            <div class="login-flex">
+                <label>Leeftijd</label>
+            </div>
+                <input type="number" name="age" value="Leeftijd">
+                <div class="login-form-buttons">
+                <button class="login-form-submit" name="submit" type="submit" >Registeer</button>
+            </div>
         </form>
     </div>
 </div>
