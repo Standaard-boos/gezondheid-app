72beb8af5be85ae762dafde2fe767eaefa172a02
Andy <496993@edu.rocmn.nl>
2019-09-18 09:35:15 +0200

    Changed some divs

--- application/view/pages/register.php
+++ application/view/pages/register.php
@@ -1,8 +1,7 @@
 <div class="container-login">
-<div class="login-header">
-    <h1>Persoon Registeren</h1>
-</div>
-    <div>
+    <div class="login-header">
+        <h1>Persoon Registeren</h1>
+    </div>
         <form class="login-form">
             <div class="login-flex">
                 <i class="fas fa-user-circle icons-input-left"></i>
@@ -13,7 +12,17 @@
                 <input type="password" name="password" class="login-inputs" placeholder="Wachtwoord" required><br>
                 <i class="fas fa-eye icons-input-right"></i>
             </div>
+            <div class="login-flex">
+                <input type="number" name="age" value="Leeftijd">
+                <div class="login-form-buttons">
+                <button class="login-form-submit" name="submit" type="submit" >Registeer</button>
+            </div>
         </form>
+</div>
+<div class="container-login">
+    <div class="login-header">
+        <h1>Eigen waarden toevoegen</h1>
+    </div>
         <form class="login-form" name="newForm" method="get" onSubmit="formAction(this)">
             <div class="login-flex">
             <label>Geslacht</label>
@@ -36,7 +45,6 @@
                 <div class="login-form-buttons">
                 <button class="login-form-submit" name="submit" type="submit" >Registeer</button>
             </div>
-        </form>
-    </div>
+    </form>
 </div>
 
