f0d1384ca6bf76ddefa93bc1c6584194a2463493
Semi <semicayci@gmail.com>
2019-09-19 08:45:21 +0200

    Added registration backend file

---
+++ application/controller/classes/registration.php
@@ -0,0 +1,16 @@
+<?php
+
+
+    class registration
+    {
+        protected $username;
+        protected $email;
+        protected $password;
+        protected $weight;
+        protected $height;
+        protected $age;
+        protected $gender;
+
+
+
+    }
