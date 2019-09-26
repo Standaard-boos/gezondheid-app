46750889bf88ba1429618220d6fbbcd834182878
Thomas Visch <thomasvisch@outlook.com>
2019-09-26 08:44:12 +0200

    add charstjs

--- public_html/assets/js/chart.js
+++ public_html/assets/js/chart.js
@@ -15,14 +15,10 @@ function ShowHideCharts() {
         case "gewicht":
             document.getElementById("chart1").classList.add("hidden");
             document.getElementById("chart2").classList.remove("hidden");
-            break;           
-               
+            break;                          
     }
 
-
 }
-
-
 
 var ctx = document.getElementById('myChart').getContext('2d');
 var myChart = new Chart(ctx, {
@@ -37,8 +33,6 @@ var myChart = new Chart(ctx, {
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)'
-
-
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
@@ -73,8 +67,7 @@ var myChart2 = new Chart(ctx2, {
             fill: false
 
         }]
-
-        
+       
     },
     options: {
         title: {
