fc76784554c36ae3f521a467cfee8b8a58564336
Semi <semicayci@gmail.com>
2019-09-30 09:37:17 +0200

    Fixed styling (WIP)

--- application/view/pages/waarden.php
+++ application/view/pages/waarden.php
@@ -7,21 +7,21 @@ $insertValues->insertValues();
 ?>
 <div class="container-form">
     <h1 class="title">Gegevens invoegen</h1>
-    <form class="waarden-form" action="/waarden" method="post">
-        <div class="waarden-flex">
-            <input class="waarden-inputs" id="voedsel" type="text" name="voedsel" placeholder="Eten of drinken">
+    <form class="form" action="/waarden" method="post">
+        <div>
+            <input class="input" id="voedsel" type="text" name="voedsel" placeholder="Eten of drinken">
             <i class="icon-right fas fa-utensils"></i>
         </div>
-        <div class="waarden-flex">
+        <div>
             <h2>Waardes in grammen</h2>
-            <input class="waarden-inputs" id="calorieen" type="text" name="calorieen" placeholder="Calorieen">
-            <input class="waarden-inputs" id="koolydraten" type="text" name="koolhydraten" placeholder="Koolydraten">
-            <input class="waarden-inputs" id="eiwitten" type="text" name="eiwitten" placeholder="Eiwitten">
+            <input class="input" id="calorieen" type="text" name="calorieen" placeholder="Calorieen">
+            <input class="input" id="koolydraten" type="text" name="koolhydraten" placeholder="Koolydraten">
+            <input class="input" id="eiwitten" type="text" name="eiwitten" placeholder="Eiwitten">
         </div>
         <div class="waarden-flex">
-            <input class="waarden-inputs" id="vetten" type="text" name="vetten" placeholder="Vetten ">
-            <input class="waarden-inputs" id="suikers" type="text" name="suikers" placeholder="Suikers">
-            <input class="waarden-inputs" id="zout" type="text" name="zout" placeholder="Zout">
+            <input class="inputs" id="vetten" type="text" name="vetten" placeholder="Vetten ">
+            <input class="input" id="suikers" type="text" name="suikers" placeholder="Suikers">
+            <input class="input" id="zout" type="text" name="zout" placeholder="Zout">
         </div>
         <button class="button" id="submit_gegevens" name="submit" type="submit">Gegevens toevoegen</button>
     </form>
