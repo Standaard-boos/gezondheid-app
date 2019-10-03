59664f9a50a385343d3d5e3e1daf1250433bec65
Nuro <nuro0ok@gmail.com>
2019-10-03 15:19:48 +0200

    text

--- application/view/error/404.php
+++ application/view/error/404.php
@@ -7,12 +7,12 @@
                 stroke-width="0.1"
                 points="0.9,0.1 0.1,0.5 0.9,0.9"
         />
-    </svg> Home
+    </svg> Terug
 </a>
 <div class="fourbackground-wrapper">
     <h1 id="visual">404</h1>
 </div>
-<p>The page you’re looking for does not exist.</p>
+<p>Pagina niet gevonden</p>
 </body>
 
 
