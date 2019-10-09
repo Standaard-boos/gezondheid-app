75768cf03e767ef572cb42e92e908901cc0b561b
Bashar <basharbasil1234@outlook.com>
2019-10-09 13:56:44 +0200

    add goals page is fully done with validation, sorry for commiting in the development branche :)

--- application/controller/classes/addGoals.php
+++ application/controller/classes/addGoals.php
@@ -17,12 +17,34 @@
         
         public function addGoal(){
             if(isset($_POST['submit'])){
-                $this->task = $this->db->connection->real_escape_string($_POST['task']) ?? 'not defined';
+                $this->task = $this->db->connection->real_escape_string(strtolower($_POST['task'])) ?? 'not defined';
                 $this->task_quantity = $this->db->connection->real_escape_string($_POST['task_quantity']) ?? 'not defined';
-                $this->db->query('INSERT INTO user_goals (user_id,task_quantity)
-                    VALUES (?,?)',$this->user_id, $this->task_quantity);
-                $this->db->query('INSERT INTO goals (task)
-                    VALUES (?)',$this->task);
+                
+                $stmtCheck = $this->db->connection->prepare('SELECT `id` FROM goals WHERE task = ?');
+                $stmtCheck->bind_param('s',$this->task);
+                $stmtCheck->bind_result($id);
+                $stmtCheck->execute();
+                
+                //check if goal already exists 
+                if($stmtCheck->fetch() > 0){
+                    $stmtCheck->close();
+                    // insert in user_goals table
+                    $this->db->query('INSERT INTO user_goals (user_id,task_quantity,goals_id)
+                    VALUES (?,?,?)',$this->user_id, $this->task_quantity,$id);
+                }else{
+                    //insert in goals table
+                    $stmt = $this->db->connection->prepare('INSERT INTO goals (task)
+                        VALUES(?);');
+                    $stmt->bind_param('s', $this->task);
+                    $stmt->execute();
+                    $last_id = $this->db->connection->insert_id;
+
+                    // // insert in user_goals table
+                    $this->db->query('INSERT INTO user_goals (user_id,task_quantity,goals_id)
+                        VALUES (?,?,?)',$this->user_id, $this->task_quantity,$last_id);
+
+                    $stmt->close();
+                }     
             }
         }
     }
