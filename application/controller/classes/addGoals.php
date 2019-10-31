6b53e34e2f19b23f1d349bf87fb73171a35fc390
Nuro <nuro0ok@gmail.com>
2019-10-31 01:08:29 +0100

    Feature added goals dropdown
    Goals have a dropdown now with sets behind them.

--- application/controller/classes/addGoals.php
+++ application/controller/classes/addGoals.php
@@ -9,7 +9,7 @@
         public function __construct($db){
             $this->db = $db;
             $this->user_id = $_SESSION['user_id'];
-            
+
             if(!isset($_SESSION['loggedin'])){
                 header("Location:/");
             }
@@ -21,33 +21,29 @@
                 $this->task = $this->db->connection->real_escape_string(strtolower($_POST['task'])) ?? 'not defined';
                 $this->task_quantity = $this->db->connection->real_escape_string($_POST['task_quantity']) ?? 'not defined';
 
-                $stmtCheck = $this->db->connection->prepare('SELECT `id` FROM goals WHERE task = ?');
+                $stmtCheck = $this->db->connection->prepare('SELECT id, task, sets  FROM goals WHERE id = ? ');
                 $stmtCheck->bind_param('s',$this->task);
-                $stmtCheck->bind_result($id);
+                $stmtCheck->bind_result($id, $goalname, $sets);
                 $stmtCheck->execute();
 
-                //check if goal already exists 
+                $stmtCheck->fetch();
+                $timestamp = date('Y-m-d H:i:s');
+                //check if goal already exists
                 if($stmtCheck->fetch() > 0){
                     $stmtCheck->close();
                     // insert in user_goals table
-                    $this->db->query('INSERT INTO user_goals (user_id,task_quantity,user_progress,goals_id, display)
-                    VALUES (?,?,0,?,?)',$this->user_id, $this->task_quantity,$id, 1);
+                    $this->db->query('INSERT INTO user_goals (user_id, date, goal_waarden, task_quantity,user_progress,goals_id, display)
+                        VALUES (?,?,?,?,?,?,?)',$this->user_id, $timestamp, $sets,  $this->task_quantity, 0, $this->task, 1);
                     $_SESSION['addgoalsuccess'] = 'Doel toegevoegd!';
                 }else{
-                    //insert in goals table
-                    $stmt = $this->db->connection->prepare('INSERT INTO goals (task)
-                        VALUES(?);');
-                    $stmt->bind_param('s', $this->task);
-                    $stmt->execute();
-                    $last_id = $this->db->connection->insert_id;
                     // // insert in user_goals table
-                    $this->db->query('INSERT INTO user_goals (user_id,task_quantity,user_progress,goals_id, display)
-                        VALUES (?,?,0,?,?)',$this->user_id, $this->task_quantity,$last_id, 1);
+                    $this->db->query('INSERT INTO user_goals (user_id, date, goal_waarden, task_quantity,user_progress,goals_id, display)
+                        VALUES (?,?,?,?,?,?,?)',$this->user_id, $timestamp, $sets,  $this->task_quantity, 0, $this->task, 1);
 
                     $_SESSION['addgoalsuccess'] = 'Doel toegevoegd!';
 
-                    $stmt->close();
-                }     
+//                    $stmt->close();
+                }
             }
         }
                
