d0e0f3076b67facccf1ccdd6bea69e3219b9e8b1
Thomas Visch <thomasvisch@outlook.com>
2019-11-07 22:49:15 +0100

    update drug.php
    add $score = 10

--- application/controller/classes/drugs.php
+++ application/controller/classes/drugs.php
@@ -31,5 +31,54 @@ class Drugs{
                 }
             }
 
+        }
+    }
+
+    public function calculateDrugs()
+    {
+        $drugscore = $this->db->query('SELECT drugspoints FROM drugs WHERE user_id = ?', $_SESSION['user_id'])->fetchAll();
+
+        foreach ($drugscore as $drug) {
+            $newDrugscore[] = array_sum($drug);
+            $newscore = array_sum($newDrugscore);
+        }
+
+        $score = 10;
+
+        switch (true) {
+            case($newscore >= 10):
+                $score = 1;
+                break;
+            case($newscore == 9):
+                $score = 1;
+                break;
+            case($newscore == 8):
+                $score = 2;
+                break;
+            case($newscore == 7):
+                $score = 3;
+                break;
+            case($newscore == 6):
+                $score = 4;
+                break;
+            case($newscore == 5):
+                $score = 5;
+                break;
+            case($newscore == 4):
+                $score = 6;
+                break;
+            case($newscore == 3):
+                $score = 7;
+                break;
+            case($newscore == 2):
+                $score = 8;
+                break;
+            case($newscore == 1):
+                $score = 9;
+                break;
+        }
+
+        return $score;
+
     }
 }
