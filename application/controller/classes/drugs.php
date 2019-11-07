8cbb2911d720f4f102c6a60474cff82c8888ed13
Nuro <nuro0ok@gmail.com>
2019-11-07 20:46:33 +0100

    Added drugs points, adding drugs will now put drug points into the database

--- application/controller/classes/drugs.php
+++ application/controller/classes/drugs.php
@@ -21,12 +21,74 @@ class Drugs{
                     $timestamp = date('Y-m-d H:i:s');
                     $_SESSION['addeddrugs'] = 'Uw drugs data is opgeslagen!';
 
+                    switch (true) {
+                        case($_POST['drug_types'] == 'Speed'):
+                            $points = 5;
+                            $drugtype = 1;
+                            break;
+
+                        case($_POST['drug_types'] == 'Cocaine'):
+                            $points = 7;
+                            $drugtype = 2;
+                            break;
+
+                        case($_POST['drug_types'] == 'GHB'):
+                            $points = 2;
+                            $drugtype = 3;
+                            break;
+
+                        case($_POST['drug_types'] == 'Cannabis'):
+                            $points = 3;
+                            $drugtype = 4;
+                            break;
+
+                        case($_POST['drug_types'] == 'Heroine'):
+                            $points = 10;
+                            $drugtype = 5;
+                            break;
+
+                        case($_POST['drug_types'] == 'LSD'):
+                            $points = 1;
+                            $drugtype = 6;
+                            break;
+
+                        case($_POST['drug_types'] == 'Methadon'):
+                            $points = 2;
+                            $drugtype = 7;
+                            break;
+
+                        case($_POST['drug_types'] == 'Paddo'):
+                            $points = 4;
+                            $drugtype = 8;
+                            break;
+
+                        case($_POST['drug_types'] == 'XTC'):
+                            $points = 3;
+                            $drugtype = 9;
+                            break;
+
+                        case($_POST['drug_types'] == 'Ketamine'):
+                            $points = 5;
+                            $drugtype = 10;
+                            break;
+
+                        default:
+                            return "Geen data gevonden";
+                    }
+
+                    if(!EMPTY($_POST['input_quantity'])){
+                        $newPoints = $points * $_POST['input_quantity'];
+                    }
+                    else{
+                        $newPoints = $points * $_POST['quantity'];
+                    }
+
                     if (empty($input_quantity)) {
-                        $this->db->query('INSERT INTO drugs (user_id,drugs_type_id,quantity,created_at)
-                        VALUES (?,?,?,?)',$_SESSION['user_id'],$drug_type, $quantity, $timestamp);
+                        $this->db->query('INSERT INTO drugs (user_id,drugs_type_id,quantity,drugspoints,created_at)
+                        VALUES (?,?,?,?,?)',$_SESSION['user_id'],$drugtype, $quantity, $newPoints, $timestamp);
                     }else{
-                        $this->db->query('INSERT INTO drugs (user_id,drugs_type_id,quantity)
-                        VALUES (?,?,?)',$_SESSION['user_id'],$drug_type, $input_quantity);
+                        $this->db->query('INSERT INTO drugs (user_id,drugs_type_id,quantity,drugspoints)
+                        VALUES (?,?,?,?)',$_SESSION['user_id'],$drugtype, $input_quantity, $newPoints);
                     }
                 }
             }
--- application/view/pages/drugs.php
+++ application/view/pages/drugs.php
@@ -25,29 +25,29 @@ if(isset($_SESSION['addeddrugs']))
         <div class="input-icon">
             <select class="input" name="drug_types" required>
                 <option value="">Kies uit...</option>
-                <option value="1">Speed</option> 
-                <option value="2">Cocaïne</option> 
-                <option value="3">GHB</option> 
-                <option value="4">Cannabis (hasj en wiet)</option> 
-                <option value="5">Heroïne</option> 
-                <option value="6">LSD</option> 
-                <option value="7">Methadon</option> 
-                <option value="8">Paddo’s en truffels</option> 
-                <option value="9">XTC</option> 
-                <option value="10">Ketamine</option> 
+                <option value="Speed">Speed</option>
+                <option value="Cocaine">Cocaïne</option>
+                <option value="GHB">GHB</option>
+                <option value="Cannabis">Cannabis (hasj en wiet)</option>
+                <option value="Heroine">Heroïne</option>
+                <option value="LSD">LSD</option>
+                <option value="Methadon">Methadon</option>
+                <option value="Paddo">Paddo’s en truffels</option>
+                <option value="XTC">XTC</option>
+                <option value="Ketamine">Ketamine</option>
             </select>
         </div>
         <h4 class="input-icon">Hoeveel heb je genomen? (Pil, Spuit, lijntje, gram)</h4>
         <div class="input-icon">
             <select class="input" name="quantity" id="select_quantity" required>
                 <option value="">Kies uit...</option>
-                <option value="0.25">1/4</option> 
-                <option value="0.50">1/2</option> 
-                <option value="0.75">3/4</option> 
-                <option value="1.00">1 </option> 
-                <option value="2">2</option> 
-                <option value="3">3</option> 
-                <option value="more">meer dan 3</option> 
+                <option value="0.25">1/4</option>
+                <option value="0.50">1/2</option>
+                <option value="0.75">3/4</option>
+                <option value="1.00">1 </option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="more">3 of meer?</option>
             </select>
         </div>
         <div class="input-icon">
